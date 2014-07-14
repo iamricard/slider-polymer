26a033bf0635d3ad21e89b03d7bce6678ba3d1d3
Ricard Sole <ricard.solecasas@gmail.com>
2014-07-14 19:52:15 +0200

    started working on the horizontal slider
    There's still a lot of work to be done in the horizontal animation,
    but it kind of works now. I have to fix what happens when it reaches
    the end. For now the base animation and the horizontal animation are
    in different branches because it was easier to work on them
    separately.

--- main.js
+++ main.js
@@ -15,10 +15,6 @@
       for (var img in this.images) {
         var currentImg = parseInt(img, 10);
 
-        if (currentImg === this.current) {
-          this.images[currentImg].classList.add('show');
-        }
-
         this.images[currentImg].classList.add('slider-img');
 
         container.appendChild(this.images[img]);
@@ -30,14 +26,16 @@
       this.current = parseInt(this.current, 10);
       switch (next) {
         case 'left':
-          this.images[this.current].classList.remove('show');
+          this.images[this.current].classList.add('left-slider');
           this.current = this.current === 0 ? this.images.length - 1 : this.current - 1;
-          this.images[this.current].classList.add('show');
+          this.images[this.current].classList.remove('right-slider');
+          this.images[this.current].classList.remove('left-slider');
           break;
         case 'right':
-          this.images[this.current].classList.remove('show');
+          this.images[this.current].classList.add('right-slider');
           this.current = this.current === this.images.length - 1 ? 0 : this.current + 1;
-          this.images[this.current].classList.add('show');
+          this.images[this.current].classList.remove('right-slider');
+          this.images[this.current].classList.remove('left-slider');
           break;
         default:
           this.images[this.current].classList.remove('show');
